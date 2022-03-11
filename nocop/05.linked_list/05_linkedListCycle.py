b01067dd0dcaa0153945fca318cf88837ef4d5e8
chris-y <maxsummer256@gmail.com>
2022-03-11 21:42:05 +0900

    add: nocop/linked_list/06

--- nocop/05.linked_list/05_linkedListCycle.py
+++ nocop/05.linked_list/05_linkedListCycle.py
@@ -1,11 +1,19 @@
 """
 <basic>
-[1,3,5,7,9]이라는 노드 리스트에 루프가 있는지 확인하는 문제입니다.
+[1,3,5,7,9]이라는 노드 리스트에 루프가 있으면 True를 반환하는 문제입니다.
 
 hash set 사용:
-노드 리스트를 순회하며 hash set에 
+노드 리스트를 순회하며 현재 노드가 hash set에 없는 경우 넣어두고 다음 노드로 포인트를 옮기는데
+만약 다음 노드가 set 안에 있다면 루프가 형성되어있다는 의미이므로 True가 됩니다.
+TC는 노드를 순회하는 O(n)이, SC는 hash set을 저장하는 O(n)이 필요하게 됩니다.
 
-
+fast & slow pointer:
+각각 한칸씩 이동하는 포인터와 두칸씩 이동하는 포인터를 만들고 순회시키면 만약 루프가 존재하는 경우
+두 포인터가 루프 안에서 돌게 되고 언젠가 서로 만나게 되므로 루프가 존재한다는 증명이 됩니다.
+fast.next가 존재하지 않는 경우 반복을 종료하게 되는 조건을 걸고 반복이 종료된다면 루프가 없다는 의미가 됩니다.
+TC는 fast 포인터가 루프시작점에 도달하는 시간 O(n) + fast가 slow를 잡는데 걸리는 시간 O(k)가 되고 k = 루프 안 노드의 갯수인데 
+k가 가질 수 있는 최대 노드의 갯수를 n이 되기 때문에 O(n) + O(n) 즉, 최종 TC는 O(n)이 됩니다.
+SC는 추가 공간을 사용하지 않았기 때문에 O(1)이 됩니다.
 """
 
 from typing import List
@@ -62,7 +70,7 @@ class HasLoop:
             if fast_node.next:
                 slow_node = slow_node.next
                 fast_node = fast_node.next.next
-            else:
+            else: # 루프가 존재하지 않으면 node.next가 있을 수 없으므로 False
                 break
 
             if fast_node == slow_node:
---
+++ nocop/05.linked_list/06_intersectionOfTwoList.py
@@ -0,0 +1,101 @@
+"""
+<basic>
+1,3,5,7,9와 2,7,9 라는 링크드리스트가 주어지면 교차점인 7을 리턴하는 문제입니다.
+
+TC O(n), SC O(n):
+current 포인터를 만들고 하나의 링크드리스트를 순회하며(TC O(n)) 노드들의 address들을 해시셋에 저장(SC O(n))하고 
+다시 current 포인터를 다음 노드를 순회(TC O(n)하게 하면서 해시셋과 비교하며 교차점(공통된 값)을 찾으면 됩니다.
+
+TC O(n), SC O(1):
+해시셋을 사용하지 않는 방법입니다.
+두 링크드리스트에 각각의 포인터를 지정해주고 한칸씩 이동하며 끝지점에 도달할 경우 상대방 노드의 헤드로 이동하게 합니다.
+예시의 링크드리스트 둘은 길이의 차이가 있는데 1,3,5,7,9과 2,7,9는 아래와 같이 두 개의 길이차이가 있고
+#list a 1-3-5 \
+#              7 - 9
+#list b     2 /
+list a가 1에서 9까지 도달하는데 4회 이동하고 list b의 2로 교차이동하는데 1회 7까지 이동하는데 1회 총 6회의 이동이 필요하며
+list b가 9까지 이동하는데 2회 list a의 1로 교차이동하는데 1회 이후 7까지 3회 총 6회의 이동이 필요합니다.
+서로 교차하기 때문에 링크드리스트간의 길이차이는 사라지고 곧 만나는 지점이 생기게 되는데 이 부분이 교차점이 됩니다.
+"""
+
+from typing import List
+
+
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def create_list(list: List[int]) -> ListNode:
+    if len(list) == 0:
+        return RuntimeError('No elem')
+    
+    head_node = ListNode(list[0])
+    last_node = head_node
+    for idx in range(1, len(list)):
+        # print(last_node.val, last_node.next)
+        node = ListNode(list[idx])
+        last_node.next = node
+        # print(last_node.val, last_node.next.val)
+        last_node = node
+    return head_node
+
+def get_idx_node(head: ListNode, idx: int):
+    nth_node = head
+    for _ in range(0, idx):
+        nth_node = nth_node.next
+    return nth_node
+
+# intersection 만들어주기
+#list a 1-3-5 \
+#              7 - 9
+#list b     2 /
+list1 = create_list([1,3,5,7,9])
+list2 = ListNode(2)
+intersection = get_idx_node(list1, 3)
+list2.next = intersection
+
+class IntersrctionFinder:
+    def two_node_way(self, head_a: ListNode, head_b: ListNode) -> ListNode:
+        node_a = head_a
+        node_b = head_b
+        while node_a != node_b:
+            if node_a:
+                node_a = node_a.next
+            else:
+                node_a = head_b
+
+            if node_b:
+                node_b = node_b.next
+            else:
+                node_b = head_a
+        
+        return node_a
+
+    def hash_way(self, head_a: ListNode, head_b: ListNode) -> ListNode:
+        hash_set = set()
+
+        current_head = head_a
+        while current_head:
+            hash_set.add(current_head)
+            current_head = current_head.next
+        # for i in hash_set:
+        #     if i.next != None:
+        #         print(i.val, i.next.val)
+        #     else:
+        #         i.next = 'end_node'
+        #         print(i.val, i.next)
+
+        current_head = head_b
+        while current_head:
+            if current_head in hash_set:
+                return current_head.val
+            else:
+                current_head = current_head.next
+
+        return -1
+
+finder = IntersrctionFinder()
+
+print(finder.two_node_way(list1, list2).val)
+print(finder.hash_way(list1, list2))
