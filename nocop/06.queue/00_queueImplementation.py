7313cfcbc7ab7d82e8e302a3ba6581102beb84e6
chris-y <maxsummer256@gmail.com>
2022-03-13 22:33:14 +0900

    add: nocop/link/queue/00

---
+++ nocop/06.queue/00_queueImplementation.py
@@ -0,0 +1,51 @@
+"""
+queue 자료형의 구현은 다음과 같습니다.
+"""
+
+class QueueNode:
+    def __init__(self, val: int):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Queue:
+    def __init__(self):
+        self._head = QueueNode(-1)
+        self._tail = QueueNode(-1)
+        self._head.right = self._tail
+        self._tail.left = self._head
+
+    def enqueu(self, val: int):
+        new_node = QueueNode(val)
+        last_node = self._tail.left
+
+        last_node.right = new_node
+        new_node.left = last_node
+
+        self._tail.left = new_node
+        new_node.right = self._tail
+
+    def dequeqe(self) -> int:
+        first_node = self._head.right
+
+        if first_node == self._tail:
+            raise RuntimeError('no elem')
+
+        value = first_node.val
+        second_node = first_node.right
+        self._head.right = second_node
+        second_node.left = self._head
+
+        return value
+
+
+q = Queue()
+
+q.enqueu(1)
+q.enqueu(2)
+q.enqueu(3)
+
+print(q.dequeqe())
+print(q.dequeqe())
+print(q.dequeqe())
+print(q.dequeqe())
